60f9c7c617fa2b09c7d23649676d3570d13a58c4
54sword <54sword@gmail.com>
2017-10-16 10:16:07 +0800

    更新代码

--- app/api/v1/phone.js
+++ app/api/v1/phone.js
@@ -1,18 +1,30 @@
 
 import { User, Captcha, Phone } from '../../models'
-var async = require('async');
+import async from 'async'
+import Countries from '../../data/countries'
 
 exports.reset = function(req, res, next) {
 
   var user = req.user
   var phone = req.body.phone
   var captcha = req.body.captcha
+  var areaCode = req.body.area_code
 
   async.waterfall([
 
     function(callback) {
-      if (!phone || !captcha) {
+
+      // 判断区号是否有效
+      let existAreaCode = false
+
+      Countries.map(item=>{
+        if (item.code == areaCode) existAreaCode = true
+      })
+
+      if (!phone || !captcha || !areaCode) {
         callback(10005)
+      } else if (!existAreaCode) {
+        callback(30004)
       } else {
         callback(null)
       }
@@ -36,7 +48,7 @@ exports.reset = function(req, res, next) {
     function(account, callback) {
 
       Captcha.findOne({
-        query: { user_id: user._id, phone },
+        query: { user_id: user._id, phone, area_code: areaCode },
         options: { sort:{ create_at: -1 } },
         callback: (err, _captcha) => {
           if (err) console.log(err)
@@ -44,7 +56,7 @@ exports.reset = function(req, res, next) {
 
             Phone.update({
               condition: { _id: account._id },
-              contents: { phone },
+              contents: { phone, area_code: areaCode },
               callback: function(err){
                 if (err) console.log(err)
 
@@ -83,12 +95,32 @@ exports.binding = function(req, res, next) {
   var user = req.user;
   var phone = req.body.phone;
   var captcha = req.body.captcha;
+  let areaCode = req.body.area_code;
 
   async.waterfall([
 
+    (callback) => {
+
+      // 判断区号是否有效
+      let existAreaCode = false
+      
+      Countries.map(item=>{
+        if (item.code == areaCode) existAreaCode = true
+      })
+
+      if (!phone || !captcha || !areaCode) {
+        callback(10005)
+      } else if (!existAreaCode) {
+        callback(30004)
+      } else {
+        callback(null)
+      }
+    },
+
+    // 验证码验证码是否正确
     function(callback) {
       Captcha.findOne({
-        query: { user_id: user._id, phone },
+        query: { user_id: user._id, phone, area_code: areaCode },
         options: { sort:{ create_at: -1 } },
         callback: (err, _captcha) => {
           if (_captcha && _captcha.captcha == captcha) {
@@ -99,6 +131,7 @@ exports.binding = function(req, res, next) {
       })
     },
 
+    // 验证手机号是否存在
     function(callback) {
 
       Phone.findOne({
@@ -120,11 +153,12 @@ exports.binding = function(req, res, next) {
       Phone.save({
         data: {
           user_id: user._id,
-          phone: phone
+          phone: phone,
+          area_code: areaCode
         },
         callback: (err, acc) => {
           if (err) console.log(err)
-          
+
           // 删除该用户所有的手机手机验证码
           Captcha.remove({ condition: { user_id: user._id, phone } })
 
